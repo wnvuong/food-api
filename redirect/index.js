bc475a7ee6bd0498114a9f7acf6c00e03ffe6df5
William Vuong <wnvuong@vt.edu>
2016-07-16 20:18:31 -0400

    set the correct port for redirect, and only add params if not null

--- redirect/index.js
+++ redirect/index.js
@@ -1,7 +1,13 @@
+const secretProperties = require('../secretProperties');
+
 exports.register = (plugin, options, next) => {
 
   const redirect = function (request, reply) {
-    return reply.redirect('https://' + request.info.host + '/' + request.params.path);
+    var params = null;
+    if (request.params.path != null) {
+      params = '/' + request.params.path;
+    }
+    return reply.redirect('https://' + request.info.hostname + ':' + secretProperties.HTTPS_PORT + (params != null ? params : ''));
   };
 
   plugin.route([
